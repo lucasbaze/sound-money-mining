bbf767cd6e85f27953c5e7b932294058bbefb610
lucas <lucas.bazemore@republic.co>
2023-06-03 21:08:29 -0500

    Update home page

--- lib/info.tsx
+++ lib/info.tsx
@@ -1,22 +1,32 @@
-import me from '../app/avatar.jpg';
+import me from "../app/avatar.jpg";
 
-export const name = 'Lee Robinson';
+export const name = "Sound Money Mining";
 export const avatar = me;
 export const about = () => {
   return (
     <>
-      Hey, I'm Lee. I'm the <b>VP of Developer Experience at Vercel</b> where my
-      team helps developers build a faster web.
+      Welcome to Sound Money Mining. Our mission is to bring <b>sound money</b>{" "}
+      back to the world.
     </>
   );
 };
 export const bio = () => {
   return (
     <>
-      I'm a Next.js contributor and help lead our open-source communities. I'm
-      passionate about frontend development and have created courses on React,
-      Next.js, and web development. I'm an advisor and investor in early stage
-      startups.
+      We believe that sound money creates the right <b>incentive structures</b>{" "}
+      for sustainable human development across the planet.
+      <br />
+      <br />
+      When money is hard to create and requires true work to generate, people
+      are incentived to make high quality long term decisions. Thus you
+      naturally get more <b>stable economies</b>, more{" "}
+      <b>realiable infrastructure</b>, and <b>thriving environments</b> for
+      humans and the planet.
+      <br />
+      <br />
+      As Bitcoin is the first working solution to digital, sound money, our
+      vision is create an entry point for anyone to invest, educate, and support
+      developments within the Bitcoin and Bitcoin mining community.
     </>
   );
 };
