ac47a4142a755bcd499d2ed4a9465a3030b559ce
lucas <lucas.bazemore@republic.co>
2023-06-04 13:47:46 -0500

    Update path configuration for posts

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -43,7 +43,7 @@ const computedFields = {
 
 export const Blog = defineDocumentType(() => ({
   name: "Blog",
-  filePathPattern: `**/*.mdx`,
+  filePathPattern: `**/*.post.mdx`,
   contentType: "mdx",
   fields: {
     title: {
