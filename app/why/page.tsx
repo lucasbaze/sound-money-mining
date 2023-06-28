664b37204156969185735eb5577a885eef431520
lucas <lucas.bazemore@republic.co>
2023-06-27 21:21:08 -0500

    add closing statement

--- app/why/page.tsx
+++ app/why/page.tsx
@@ -12,16 +12,19 @@ export default async function HomePage() {
         <p className="my-5">
 					<ol>
 						<li>We believe governments should serve the people, and not steal from them.</li>
-						<li>We believe banking the unbanked will eradicate global poverty.</li>
 						<li>We believe scarce money incentives better decision making every level of society.</li>
 						<li>We believe scarce money will make skills, labor, and materials plentiful.</li>
 						<li>We believe standarized money enables maximum resource utilzation and ultimately long term stability.</li>
 						<li>We believe monetary and economic transparency increases global optionality allowing for true globalization.</li>
 						<li>We believe decentralization drives power and decision making to the local level where it works best.</li>
 						<li>We believe war should be difficult to start and impossible to sustain for very long.</li>
+						<li>We believe banking the unbanked will eradicate global poverty.</li>
 						<li>We believe self custody of your money enables true self sovereignty and individual security.</li>
 						<li>We believe people should be able to preserve their wealth for their families and future generations.</li>
 					</ol>
+				</p>
+				<p className="my-5">
+					Ultimately, we believe in hope, prosperity, and longevity for all and we believe Bitcoin helps get us their.
 				</p>
       </div>
 
