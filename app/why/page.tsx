492283fb0ebc0dd1ea679704fb0d8e8e5323f37d
lucas <lucas.bazemore@republic.co>
2023-06-27 21:18:48 -0500

    why page

---
+++ app/why/page.tsx
@@ -0,0 +1,30 @@
+export default async function HomePage() {
+  return (
+    <section className="max-w-[512px]">
+      <h1 className="font-bold text-3xl font-serif">Why</h1>
+      <div className="prose prose-neutral dark:prose-invert text-neutral-800 dark:text-neutral-200">
+        <p className="my-5">
+					Thank you for coming to this page. I'm glad you're a curious one. 
+				</p>
+        <p className="my-5">
+					This is a list of 10 reasons on why we believe in Bitcoin and why you should study Bitcoin: 
+				</p>
+        <p className="my-5">
+					<ol>
+						<li>We believe governments should serve the people, and not steal from them.</li>
+						<li>We believe banking the unbanked will eradicate global poverty.</li>
+						<li>We believe scarce money incentives better decision making every level of society.</li>
+						<li>We believe scarce money will make skills, labor, and materials plentiful.</li>
+						<li>We believe standarized money enables maximum resource utilzation and ultimately long term stability.</li>
+						<li>We believe monetary and economic transparency increases global optionality allowing for true globalization.</li>
+						<li>We believe decentralization drives power and decision making to the local level where it works best.</li>
+						<li>We believe war should be difficult to start and impossible to sustain for very long.</li>
+						<li>We believe self custody of your money enables true self sovereignty and individual security.</li>
+						<li>We believe people should be able to preserve their wealth for their families and future generations.</li>
+					</ol>
+				</p>
+      </div>
+
+    </section>
+  );
+}
